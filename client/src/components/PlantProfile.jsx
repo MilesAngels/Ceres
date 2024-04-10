966489dd5173ff92739a2f05159a071558df0bd8
Miles Angels <milesmarieangels@gmail.com>
2024-04-09 18:35:13 -0700

    added server

--- client/src/components/PlantProfile.jsx
+++ client/src/components/PlantProfile.jsx
@@ -12,30 +12,29 @@ import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import WaterDropIcon from "@mui/icons-material/WaterDrop";
 export default function PlantProfile() {
   const apiKey = import.meta.env.VITE_PERENUAL_API_KEY;
-  
+
   // State of plant information to be displayed
   const [plant, setPlant] = useState({
-    plantName:'',
-    water: '',
-    sunlight: ''
-  })
+    plantName: "",
+    water: "",
+    sunlight: "",
+  });
 
   // State of plant data being fetched
-  const [plantInfo, setPlantInfo] = useState();
+  const [plantInfo, setPlantInfo] = useState('');
 
   useEffect(() => {
     fetch(`https://perenual.com/api/species/details/1?key=${apiKey}`)
-        .then(response => response.json())
-        .then(plantData => {
-            setPlantInfo(plantData);
-            console.log(plantData)
-        })
-  },[]);
+      .then((response) => response.json())
+      .then((plantData) => {
+        setPlantInfo(plantData);
+      });
+  }, ['']);
   return (
     <Card>
       <CardContent>
         <Avatar sx={{ margin: "auto auto" }}>
-          <img src={plantInfo.default_image.thumbnail}/>
+          <img src="" />
         </Avatar>
         <Typography>{plantInfo.common_name}</Typography>
         <Stack>
